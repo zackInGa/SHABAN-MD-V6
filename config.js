50e1709969e47dbde66f6193a550a0e075e9566a
MRSHABAN40 <shabanmr086@gmail.com>
2025-07-01 23:02:21 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -6,8 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id
-AUTO_BIO: process.env.AUTO_BIO || "false", 
+// add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
